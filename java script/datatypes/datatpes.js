9b278b5f40774b4a7978f7889f09b47941d7947d
sanjay <dubakasanjay81@gmail.com>
2026-01-08 15:10:01 +0530

    added

---
+++ java script/datatypes/datatpes.js
@@ -0,0 +1,32 @@
+//
+
+a=100
+console.log(a);
+console.log(typeof a);
+
+//
+
+a="1100";
+console.log(a);
+console.log(typeof a);
+
+//
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+//
+
+let b;
+console.log(b);
+console.log(typeof b);
+
+//
+
+a=100n;
+console.log(a);
+console.log(typeof a);
+
+//
+
---
+++ java script/variables.js
@@ -0,0 +1,3 @@
+console.log("hello world !");
+console.log("jai sree ram");
+console.log(4*2*8*2);
